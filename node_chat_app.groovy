f7af5cf6a767fd48436beea45c05a88b84ccb4ca
Ollie Thomas <ollie.thomas1992@gmail.com>
2018-09-07 13:33:39 +0100

    modified groovy

--- node_chat_app.groovy
+++ node_chat_app.groovy
@@ -1,6 +1,9 @@
 job('Node Chat App') {
     scm {
-        git('git@github.com:olliethomas1992/node-chat-app.git')
+        git('git@github.com:olliethomas1992/node-chat-app.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('olliethomas1992')
+            node / gitConfigEmail('ollie.thomas1992@gmail.com')
+        }
     }
     triggers {
         scm('H/5 * * * *')
